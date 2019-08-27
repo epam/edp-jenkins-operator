cba1eb611212224b7292656514b94f6ffcb01aec
Alexander Morozov <alexander_morozov@epam.com>
2019-08-27 11:00:54 +0300

    [EPMDEDP-3510] Init jenkins-operator with operator-sdk
    Change-Id: I92c567a81c972f2bdd88e469ca0d8d7fe40457e1

---
+++ pkg/apis/addtoscheme_v2_v1alpha1.go
@@ -0,0 +1,10 @@
+package apis
+
+import (
+	"jenkins-operator/pkg/apis/v2/v1alpha1"
+)
+
+func init() {
+	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
+	AddToSchemes = append(AddToSchemes, v1alpha1.SchemeBuilder.AddToScheme)
+}
---
+++ pkg/apis/v2/group.go
@@ -0,0 +1,6 @@
+// Package v2 contains v2 API versions.
+//
+// This file ensures Go source parsers acknowledge the v2 package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package v2
