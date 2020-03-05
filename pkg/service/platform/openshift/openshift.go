4ecf306275df946573c3e0cbdca4530424cb23c8
Serhii Shydlovskyi <serhii_shydlovskyi@epam.com>
2020-03-05 12:52:05 +0200

    [EPMDEDP-4755] Update Jenkins operator with the imagePullSecret field
    Change-Id: I7b12a2d4b335eb30216aa54a54c0934d22061dae

--- pkg/service/platform/openshift/openshift.go
+++ pkg/service/platform/openshift/openshift.go
@@ -125,6 +125,7 @@ func (service OpenshiftService) CreateDeployment(instance v1alpha1.Jenkins) erro
 					Labels: labels,
 				},
 				Spec: coreV1Api.PodSpec{
+					ImagePullSecrets: instance.Spec.ImagePullSecrets,
 					SecurityContext:               &coreV1Api.PodSecurityContext{},
 					RestartPolicy:                 coreV1Api.RestartPolicyAlways,
 					DeprecatedServiceAccount:      instance.Name,
