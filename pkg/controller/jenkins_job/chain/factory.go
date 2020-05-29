epmd-edp/jenkins-operator

[EPMDEDP-5154] Modify JenkinsJob to be able to trigger job provision
Change-Id: I94f4cde704cec4b047dc07e5506609ad4d1d0a7f

pkg/controller/jenkins_job/chain/factory.go
@@ -37,6 +37,30 @@ func CreateDefChain(s *runtime.Scheme, c *client.Client) (handler2.JenkinsJobHan
 
 }
 
+func CreateTriggerJobProvisionChain(s *runtime.Scheme, c *client.Client) (handler2.JenkinsJobHandler, error) {
+	pt := helper.GetPlatformTypeEnv()
+	ps, err := platform.NewPlatformService(pt, s, c)
+	if err != nil {
+		return nil, err
+	}
+	cs := openshift.CreateOpenshiftClients()
+	cs.Client = *c
+
+	return PutClusterProject{
+		next: PutRoleBinding{
+			next: TriggerJobProvision{
+				cs: *cs,
+				ps: ps,
+			},
+			cs: *cs,
+			ps: ps,
+		},
+		cs: *cs,
+		ps: ps,
+	}, nil
+
+}
+
 func nextServeOrNil(next handler2.JenkinsJobHandler, jj *v1alpha1.JenkinsJob) error {
 	if next != nil {
 		return next.ServeRequest(jj)

pkg/controller/jenkins_job/jenkins_job_controller.go
@@ -8,6 +8,7 @@ import (
 	jenkinsClient "github.com/epmd-edp/jenkins-operator/v2/pkg/client/jenkins"
 	"github.com/epmd-edp/jenkins-operator/v2/pkg/controller/helper"
 	"github.com/epmd-edp/jenkins-operator/v2/pkg/controller/jenkins_job/chain"
+	handler2 "github.com/epmd-edp/jenkins-operator/v2/pkg/controller/jenkins_job/chain/handler"
 	"github.com/epmd-edp/jenkins-operator/v2/pkg/service/platform"
 	"github.com/epmd-edp/jenkins-operator/v2/pkg/util/consts"
 	"github.com/epmd-edp/jenkins-operator/v2/pkg/util/finalizer"
@@ -140,9 +141,27 @@ func (r *ReconcileJenkinsJob) Reconcile(request reconcile.Request) (reconcile.Re
 		return reconcile.Result{RequeueAfter: 10 * time.Second}, nil
 	}
 
-	ch, err := chain.CreateDefChain(r.scheme, &r.client)
+	j, err := plutil.GetJenkinsInstanceOwner(r.client, i.Name, i.Namespace, i.Spec.OwnerName, i.GetOwnerReferences())
 	if err != nil {
-		return reconcile.Result{}, errors.Wrap(err, "an error has occurred while selecting chain")
+		return reconcile.Result{}, errors.Wrapf(err, "an error has been occurred while getting owner jenkins for jenkins job %v", i.Name)
+	}
+
+	var ch handler2.JenkinsJobHandler
+	jc, err := jenkinsClient.InitGoJenkinsClient(j, r.ps)
+	jobExist, err := isJenkinsJobExist(jc, i.Spec.Job.Name)
+	if err != nil {
+		return reconcile.Result{}, errors.Wrapf(err, "an error has occurred while retrieving jenkins job %v", i.Spec.Job.Name)
+	}
+	if jobExist {
+		ch, err = chain.CreateTriggerJobProvisionChain(r.scheme, &r.client)
+		if err != nil {
+			return reconcile.Result{}, errors.Wrap(err, "an error has occurred while selecting chain")
+		}
+	} else {
+		ch, err = chain.CreateDefChain(r.scheme, &r.client)
+		if err != nil {
+			return reconcile.Result{}, errors.Wrap(err, "an error has occurred while selecting chain")
+		}
 	}
 
 	if err := ch.ServeRequest(i); err != nil {
@@ -151,6 +170,17 @@ func (r *ReconcileJenkinsJob) Reconcile(request reconcile.Request) (reconcile.Re
 
 	rlog.V(2).Info("reconciling JenkinsJob has been finished")
 	return reconcile.Result{}, nil
+}
+
+func isJenkinsJobExist(jc *jenkinsClient.JenkinsClient, jp string) (bool, error) {
+	_, err := jc.GoJenkins.GetJob(jp)
+	if err != nil {
+		if err.Error() == "404" {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
 }
 
 func (r ReconcileJenkinsJob) initGoJenkinsClient(jj v2v1alpha1.JenkinsJob) (*jenkinsClient.JenkinsClient, error) {

epam/edp-jenkins-operator

pkg/controller/jenkins_job/chain/factory.go
@@ -4,7 +4,7 @@ import (
 	"github.com/epmd-edp/codebase-operator/v2/pkg/openshift"
 	"github.com/epmd-edp/jenkins-operator/v2/pkg/apis/v2/v1alpha1"
 	"github.com/epmd-edp/jenkins-operator/v2/pkg/controller/helper"
-	handler2 "github.com/epmd-edp/jenkins-operator/v2/pkg/controller/jenkins_job/chain/handler"
+	jobhandler "github.com/epmd-edp/jenkins-operator/v2/pkg/controller/jenkins_job/chain/handler"
 	"github.com/epmd-edp/jenkins-operator/v2/pkg/service/platform"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -13,7 +13,7 @@ import (
 
 var log = logf.Log.WithName("jenkins_job_handler")
 
-func CreateDefChain(s *runtime.Scheme, c *client.Client) (handler2.JenkinsJobHandler, error) {
+func CreateDefChain(s *runtime.Scheme, c *client.Client) (jobhandler.JenkinsJobHandler, error) {
 	pt := helper.GetPlatformTypeEnv()
 	ps, err := platform.NewPlatformService(pt, s, c)
 	if err != nil {
@@ -37,7 +37,31 @@ func CreateDefChain(s *runtime.Scheme, c *client.Client) (handler2.JenkinsJobHan
 
 }
 
-func nextServeOrNil(next handler2.JenkinsJobHandler, jj *v1alpha1.JenkinsJob) error {
+func CreateTriggerJobProvisionChain(s *runtime.Scheme, c *client.Client) (jobhandler.JenkinsJobHandler, error) {
+	pt := helper.GetPlatformTypeEnv()
+	ps, err := platform.NewPlatformService(pt, s, c)
+	if err != nil {
+		return nil, err
+	}
+	cs := openshift.CreateOpenshiftClients()
+	cs.Client = *c
+
+	return PutClusterProject{
+		next: PutRoleBinding{
+			next: TriggerJobProvision{
+				cs: *cs,
+				ps: ps,
+			},
+			cs: *cs,
+			ps: ps,
+		},
+		cs: *cs,
+		ps: ps,
+	}, nil
+
+}
+
+func nextServeOrNil(next jobhandler.JenkinsJobHandler, jj *v1alpha1.JenkinsJob) error {
 	if next != nil {
 		return next.ServeRequest(jj)
 	}

pkg/controller/jenkins_job/jenkins_job_controller.go
@@ -8,6 +8,7 @@ import (
 	jenkinsClient "github.com/epmd-edp/jenkins-operator/v2/pkg/client/jenkins"
 	"github.com/epmd-edp/jenkins-operator/v2/pkg/controller/helper"
 	"github.com/epmd-edp/jenkins-operator/v2/pkg/controller/jenkins_job/chain"
+	jobhandler "github.com/epmd-edp/jenkins-operator/v2/pkg/controller/jenkins_job/chain/handler"
 	"github.com/epmd-edp/jenkins-operator/v2/pkg/service/platform"
 	"github.com/epmd-edp/jenkins-operator/v2/pkg/util/consts"
 	"github.com/epmd-edp/jenkins-operator/v2/pkg/util/finalizer"
@@ -140,9 +141,27 @@ func (r *ReconcileJenkinsJob) Reconcile(request reconcile.Request) (reconcile.Re
 		return reconcile.Result{RequeueAfter: 10 * time.Second}, nil
 	}
 
-	ch, err := chain.CreateDefChain(r.scheme, &r.client)
+	j, err := plutil.GetJenkinsInstanceOwner(r.client, i.Name, i.Namespace, i.Spec.OwnerName, i.GetOwnerReferences())
 	if err != nil {
-		return reconcile.Result{}, errors.Wrap(err, "an error has occurred while selecting chain")
+		return reconcile.Result{}, errors.Wrapf(err, "an error has been occurred while getting owner jenkins for jenkins job %v", i.Name)
+	}
+
+	var ch jobhandler.JenkinsJobHandler
+	jc, err := jenkinsClient.InitGoJenkinsClient(j, r.ps)
+	jobExist, err := isJenkinsJobExist(jc, i.Spec.Job.Name)
+	if err != nil {
+		return reconcile.Result{}, errors.Wrapf(err, "an error has occurred while retrieving jenkins job %v", i.Spec.Job.Name)
+	}
+	if jobExist {
+		ch, err = chain.CreateTriggerJobProvisionChain(r.scheme, &r.client)
+		if err != nil {
+			return reconcile.Result{}, errors.Wrap(err, "an error has occurred while selecting chain")
+		}
+	} else {
+		ch, err = chain.CreateDefChain(r.scheme, &r.client)
+		if err != nil {
+			return reconcile.Result{}, errors.Wrap(err, "an error has occurred while selecting chain")
+		}
 	}
 
 	if err := ch.ServeRequest(i); err != nil {
@@ -151,6 +170,17 @@ func (r *ReconcileJenkinsJob) Reconcile(request reconcile.Request) (reconcile.Re
 
 	rlog.V(2).Info("reconciling JenkinsJob has been finished")
 	return reconcile.Result{}, nil
+}
+
+func isJenkinsJobExist(jc *jenkinsClient.JenkinsClient, jp string) (bool, error) {
+	_, err := jc.GoJenkins.GetJob(jp)
+	if err != nil {
+		if err.Error() == "404" {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
 }
 
 func (r ReconcileJenkinsJob) initGoJenkinsClient(jj v2v1alpha1.JenkinsJob) (*jenkinsClient.JenkinsClient, error) {