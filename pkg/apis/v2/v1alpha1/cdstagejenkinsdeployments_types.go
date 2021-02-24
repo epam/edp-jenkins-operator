8223c07e86e0d5e8292463d5e1a8138bf9c8ac95
Anton_Tarianyk <Anton_Tarianyk@epam.com>
2021-02-24 10:29:23 +0200

    [EPMDEDP-6109]: Add CDStageJenkinsDeployment API
    Change-Id: I18f31b75b7152a1f6eae160ef4aba1d1864f4c10

---
+++ pkg/apis/v2/v1alpha1/cdstagejenkinsdeployments_types.go
@@ -0,0 +1,59 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// CDStageJenkinsDeploymentSpec defines the desired state of CDStageJenkinsDeployment
+// +k8s:openapi-gen=true
+
+type CDStageJenkinsDeploymentSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	Job  string `json:"job"`
+	Tags []Tag  `json:"tags"`
+}
+
+type Tag struct {
+	Codebase string `json:"codebase"`
+	Tag      string `json:"tag"`
+}
+
+// CDStageJenkinsDeploymentStatus defines the observed state of CDStageJenkinsDeploymentStatus
+// +k8s:openapi-gen=true
+type CDStageJenkinsDeploymentStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	Status string `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// CDStageJenkinsDeployment is the Schema for the cdstagejenkinsdeployments API
+// +k8s:openapi-gen=true
+// +kubebuilder:subresource:status
+type CDStageJenkinsDeployment struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   CDStageJenkinsDeploymentSpec   `json:"spec,omitempty"`
+	Status CDStageJenkinsDeploymentStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// CDStageJenkinsDeploymentList contains a list of CDStageJenkinsDeployment
+type CDStageJenkinsDeploymentList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []CDStageJenkinsDeployment `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&CDStageJenkinsDeployment{}, &CDStageJenkinsDeploymentList{})
+}
--- pkg/apis/v2/v1alpha1/zz_generated.openapi.go
+++ pkg/apis/v2/v1alpha1/zz_generated.openapi.go
@@ -11,18 +11,21 @@ import (
 
 func GetOpenAPIDefinitions(ref common.ReferenceCallback) map[string]common.OpenAPIDefinition {
 	return map[string]common.OpenAPIDefinition{
-		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.Jenkins":                     schema_pkg_apis_v2_v1alpha1_Jenkins(ref),
-		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsScript":               schema_pkg_apis_v2_v1alpha1_JenkinsScript(ref),
-		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsScriptStatus":         schema_pkg_apis_v2_v1alpha1_JenkinsScriptStatus(ref),
-		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsServiceAccount":       schema_pkg_apis_v2_v1alpha1_JenkinsServiceAccount(ref),
-		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsServiceAccountSpec":   schema_pkg_apis_v2_v1alpha1_JenkinsServiceAccountSpec(ref),
-		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsServiceAccountStatus": schema_pkg_apis_v2_v1alpha1_JenkinsServiceAccountStatus(ref),
-		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsStatus":               schema_pkg_apis_v2_v1alpha1_JenkinsStatus(ref),
-		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsFolder":               schema_pkg_apis_v2_v1alpha1_JenkinsFolder(ref),
-		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsFolderStatus":         schema_pkg_apis_v2_v1alpha1_JenkinsFolderStatus(ref),
-		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsJob":                  schema_pkg_apis_v2_v1alpha1_JenkinsJob(ref),
-		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsJobSpec":              schema_pkg_apis_v2_v1alpha1_JenkinsJobSpec(ref),
-		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsJobStatus":            schema_pkg_apis_v2_v1alpha1_JenkinsJobStatus(ref),
+		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.Jenkins":                        schema_pkg_apis_v2_v1alpha1_Jenkins(ref),
+		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsScript":                  schema_pkg_apis_v2_v1alpha1_JenkinsScript(ref),
+		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsScriptStatus":            schema_pkg_apis_v2_v1alpha1_JenkinsScriptStatus(ref),
+		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsServiceAccount":          schema_pkg_apis_v2_v1alpha1_JenkinsServiceAccount(ref),
+		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsServiceAccountSpec":      schema_pkg_apis_v2_v1alpha1_JenkinsServiceAccountSpec(ref),
+		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsServiceAccountStatus":    schema_pkg_apis_v2_v1alpha1_JenkinsServiceAccountStatus(ref),
+		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsStatus":                  schema_pkg_apis_v2_v1alpha1_JenkinsStatus(ref),
+		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsFolder":                  schema_pkg_apis_v2_v1alpha1_JenkinsFolder(ref),
+		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsFolderStatus":            schema_pkg_apis_v2_v1alpha1_JenkinsFolderStatus(ref),
+		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsJob":                     schema_pkg_apis_v2_v1alpha1_JenkinsJob(ref),
+		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsJobSpec":                 schema_pkg_apis_v2_v1alpha1_JenkinsJobSpec(ref),
+		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.JenkinsJobStatus":               schema_pkg_apis_v2_v1alpha1_JenkinsJobStatus(ref),
+		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.CDStageJenkinsDeployment":       schema_pkg_apis_edp_v1alpha1_CDStageJenkinsDeployment(ref),
+		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.CDStageJenkinsDeploymentSpec":   schema_pkg_apis_edp_v1alpha1_CDStageJenkinsDeploymentSpec(ref),
+		"github.com/epmd-edp/jenkins-operator/pkg/apis/v2/v1alpha1.CDStageJenkinsDeploymentStatus": schema_pkg_apis_edp_v1alpha1_CDStageJenkinsDeploymentStatus(ref),
 	}
 }
 
@@ -452,5 +455,94 @@ func schema_pkg_apis_v2_v1alpha1_JenkinsJobStatus(ref common.ReferenceCallback) 
 			},
 		},
 		Dependencies: []string{},
+	}
+}
+
+func schema_pkg_apis_edp_v1alpha1_CDStageJenkinsDeployment(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "CDStageJenkinsDeployment is the Schema for the cdstagejenkinsdeployments API",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"kind": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/api-conventions.md#types-kinds",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"apiVersion": {
+						SchemaProps: spec.SchemaProps{
+							Description: "APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/api-conventions.md#resources",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"metadata": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("k8s.io/apimachinery/pkg/apis/meta/v1.ObjectMeta"),
+						},
+					},
+					"spec": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("./pkg/apis/edp/v1alpha1.CDStageJenkinsDeploymentSpec"),
+						},
+					},
+					"status": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("./pkg/apis/edp/v1alpha1.CDStageJenkinsDeploymentStatus"),
+						},
+					},
+				},
+			},
+		},
+		Dependencies: []string{
+			"./pkg/apis/edp/v1alpha1.CDStageJenkinsDeploymentSpec", "./pkg/apis/edp/v1alpha1.CDStageJenkinsDeploymentStatus", "k8s.io/apimachinery/pkg/apis/meta/v1.ObjectMeta"},
+	}
+}
+
+func schema_pkg_apis_edp_v1alpha1_CDStageJenkinsDeploymentSpec(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "CDStageJenkinsDeploymentSpec defines the desired state of CDStageJenkinsDeploymentSpec",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"job": {
+						SchemaProps: spec.SchemaProps{
+							Description: "INSERT ADDITIONAL SPEC FIELDS - desired state of cluster Important: Run \"operator-sdk generate k8s\" to regenerate code after modifying this file Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"tags": {
+						SchemaProps: spec.SchemaProps{
+							Type: []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Ref: ref("./pkg/apis/edp/v1alpha1.Tag"),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		Dependencies: []string{
+			"./pkg/apis/edp/v1alpha1.Tag"},
+	}
+}
+
+func schema_pkg_apis_edp_v1alpha1_CDStageJenkinsDeploymentStatus(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "CDStageJenkinsDeploymentStatus defines the observed state of CDStageJenkinsDeployment",
+				Type:        []string{"object"},
+			},
+		},
 	}
 }
